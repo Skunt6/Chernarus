2bda37895b0a8df2374d8a027385c253d8115924
Ross <rt6031@gmail.com>
2024-10-20 00:41:47 -0400

    added namalsk back

--- mpmissions/dayzOffline.chernarusplus/init.c
+++ mpmissions/dayzOffline.chernarusplus/init.c
@@ -4,89 +4,126 @@ void main()
   	DC_FastTravel.SpawnBoard(2, "10722.2 350.986 5678.11", "-162.716 0 0"); // Echo with m_ID = 2 in the config
 	DC_FastTravel.SpawnBoard(3, "14201.3 196.715 15285.3", "-74.1487 0 0"); // Trailside with m_ID = 3 in the config
 	DC_FastTravel.SpawnBoard(4, "1896.99 442.394 15000.9", "-135.043 0 0"); // north tisy with m_ID = 4 in the config
-	//INIT ECONOMY--------------------------------------
-	Hive ce = CreateHive();
-	if ( ce )
-		ce.InitOffline();
+	/*
+	  [Namalsk] CE init (offline)
+	*/
+	CreateHive();
+	GetHive().InitOffline();
 
-	//DATE RESET AFTER ECONOMY INIT-------------------------
+	/*
+	  [Namalsk] Mission time init
+	   after CE init to determine if storage mission type is outside of the required time-frame
+	   currently recommended time-frame is:
+		11/1 -> 11/30
+		keep in mind that gameplay features are tied to the mission date (stored in the storage) and that it SHOULD remain this period!
+	   while using:
+		day accelerated 6 times (serverTimeAcceleration=6), resulting in an average 78 min of day-time (RL)
+		night accelerated 24 times (serverNightTimeAcceleration=4), resulting in an average of 26 min of night-time (RL)
+	*/
 	int year, month, day, hour, minute;
-	int reset_month = 12, reset_day = 25;
-	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
+	GetGame().GetWorld().GetDate( year, month, day, hour, minute );
 
-	if ((month == reset_month) && (day < reset_day))
-	{
-		GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+    if ( ( month < 11 ) || ( month >= 12 ) )
+    {
+    	year = 2011;
+        month = 11;
+        day = 2;
+		
+		GetGame().GetWorld().SetDate( year, month, day, hour, minute );
 	}
-	else
-	{
-		if ((month == reset_month + 1) && (day > reset_day))
-		{
-			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-		}
-		else
-		{
-			if ((month < reset_month) || (month > reset_month + 1))
-			{
-				GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-			}
-		}
-	}
-}
+};
 
 class CustomMission: MissionServer
 {
+	override void OnInit()
+	{
+		super.OnInit();
+
+		// this piece of code is recommended otherwise event system is switched on automatically and runs from default values
+		// comment this whole block if NOT using Namalsk Survival
+		if ( m_EventManagerServer )
+		{
+			// enable/disable event system, min time between events, max time between events, max number of events at the same time
+			m_EventManagerServer.OnInitServer( true, 550, 1000, 2 );
+			// registering events and their probability
+			m_EventManagerServer.RegisterEvent( Aurora, 0.85 );
+			m_EventManagerServer.RegisterEvent( EVRStorm, 0.15 );
+			m_EventManagerServer.RegisterEvent( HeavyFog, 0.4 );
+		}
+	}
+
 	void SetRandomHealth(EntityAI itemEnt)
 	{
-		if ( itemEnt )
+		if (itemEnt)
 		{
-			float rndHlt = Math.RandomFloat( 0.45, 0.65 );
+			float rndHlt = Math.RandomFloat( 0.50, 0.85 );
 			itemEnt.SetHealth01( "", "", rndHlt );
 		}
 	}
 
-	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
-	{
-		Entity playerEnt;
-		playerEnt = GetGame().CreatePlayer( identity, characterName, pos, 0, "NONE" );
-		Class.CastTo( m_player, playerEnt );
-
-		GetGame().SelectPlayer( identity, m_player );
-
-		return m_player;
-	}
-
-	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
+	override void StartingEquipSetup( PlayerBase player, bool clothesChosen )
 	{
 		EntityAI itemClothing;
 		EntityAI itemEnt;
 		ItemBase itemBs;
 		float rand;
-
+		
+		// top
 		itemClothing = player.FindAttachmentBySlotName( "Body" );
 		if ( itemClothing )
 		{
 			SetRandomHealth( itemClothing );
-			
-			itemEnt = itemClothing.GetInventory().CreateInInventory( "BandageDressing" );
-			player.SetQuickBarEntityShortcut(itemEnt, 2);
 
-			string chemlightArray[] = { "Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red" };
-			int rndIndex = Math.RandomInt( 0, 4 );
-			itemEnt = itemClothing.GetInventory().CreateInInventory( chemlightArray[rndIndex] );
-			player.SetQuickBarEntityShortcut(itemEnt, 1);
+			itemEnt = itemClothing.GetInventory().CreateInInventory( "Rag" );
+			if ( Class.CastTo( itemBs, itemEnt ) )
+			{
+				SetRandomHealth( itemEnt );
+				itemBs.SetQuantity( 4 );
+				itemBs.SetCleanness( 1 );
+			}
+			player.SetQuickBarEntityShortcut( itemEnt, 0 );
+			
+			itemEnt = itemClothing.GetInventory().CreateInInventory( "RoadFlare" );
 			SetRandomHealth( itemEnt );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( "RoadFlare" );
+			SetRandomHealth( itemEnt );
+			player.SetQuickBarEntityShortcut( itemEnt, 1 );
 		}
-		
+
+		// pants
 		itemClothing = player.FindAttachmentBySlotName( "Legs" );
 		if ( itemClothing )
+		{
 			SetRandomHealth( itemClothing );
-		
+
+			itemEnt = itemClothing.GetInventory().CreateInInventory( "Heatpack" );
+			SetRandomHealth( itemEnt );
+
+			int throwDice = Math.RandomInt( 0, 2 );
+			if ( throwDice == 0 )
+				itemEnt = itemClothing.GetInventory().CreateInInventory( "dzn_tool_watch" );
+			else
+				itemEnt = itemClothing.GetInventory().CreateInInventory( "dzn_tool_watch2" );
+			player.SetQuickBarEntityShortcut( itemEnt, 2 );
+		}
+
+		// shoes
 		itemClothing = player.FindAttachmentBySlotName( "Feet" );
+		if ( itemClothing )
+		{
+			SetRandomHealth( itemClothing );
+		}
+
+		// bump fresh spawn water and energy values (to compensate for the frozen food and harder-to-get wells)
+		player.GetStatWater().Set( 900 );
+		player.GetStatEnergy().Set( 1100 );
+
+		// add temporal resistance against the common cold
+		player.SetTemporaryResistanceToAgent(eAgents.INFLUENZA, 900);
 	}
 };
-
+  
 Mission CreateCustomMission(string path)
 {
 	return new CustomMission();
-}
+};
